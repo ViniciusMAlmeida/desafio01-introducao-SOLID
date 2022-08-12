7c363ff0382551501deb3a92443af563e7dbfd5e
Vinicius Almeida <vinicius.sec21@gmail.com>
2022-08-12 13:08:49 -0300

    comitando desafio

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,16 +2,27 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) { }
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists!");
+        }
+
+        const user = this.usersRepository.create({ name, email });
+        delete user.id;
+        delete user.created_at;
+        delete user.updated_at;
+
+        return user;
+    }
 }
 
 export { CreateUserUseCase };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,27 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) { }
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const user = this.usersRepository.findById(user_id);
+        if (!user) {
+            throw new Error("User not found!");
+        }
+
+        const isAdmin = user.admin;
+        if (!isAdmin) {
+            throw new Error("User must be admin!");
+        }
+
+        const users = this.usersRepository.list();
+
+        return users;
+    }
 }
 
 export { ListAllUsersUseCase };
